63c0acc73d81fc0704cd91730f30258c42311284
Kang-min Liu <gugod@gugod.org>
2021-08-03 19:24:05 +0900

    replace Net::Twitter with Twitter::API
    ... for Net::Twitter is no longer being maintained.

--- bin/grab-tweets.pl
+++ bin/grab-tweets.pl
@@ -8,7 +8,7 @@ use Getopt::Long qw(GetOptions);
 
 use IO::All;
 use List::MoreUtils qw(uniq);
-use Net::Twitter;
+use Twitter::API;
 use URI;
 use XML::Feed;
 use YAML;
@@ -30,29 +30,22 @@ my $app_root = io->catdir(@dirs);
 
 sub grab_tweets {
     my ($keyword) = @_;
-    my $t = Net::Twitter->new(
-        traits => ['API::RESTv1_1'],
+    my $t = Twitter::API->new_with_traits(
+        traits => 'Enchilada',
         consumer_key        => $config->{consumer_key},
         consumer_secret     => $config->{consumer_secret},
         access_token        => $config->{access_token},
         access_token_secret => $config->{access_token_secret},
     );
-    my $r;
-    my @tweets;
 
-    $r = $t->search( $keyword );
-    push @tweets, grep {
+    my $r = $t->search( $keyword );
+
+    return grep {
         !/(http|@\S+)/
     } map {
         $_->{text}
     } @{$r->{statuses}};
-
-    @tweets = uniq sort @tweets;
-
-    return @tweets;
 }
-
-my @new_tweets;
 
 my @query = qw(咖啡 不賴);
 
@@ -61,13 +54,12 @@ for my $q (@ARGV) {
     unshift @query, $q;
 }
 
-for my $q (@query) {
-    push @new_tweets, grab_tweets($q);
-}
+my @new_tweets = map { grab_tweets($_) } @query;
 
 my @old_tweets = $app_root->catfile("corpus", "tweets.txt")->assert->utf8->chomp->getlines;
 
-my @tweets = sort { length($b) <=> length($a) } map {
+my @tweets = sort { length($b) <=> length($a) } uniq map {
+    s/\t/ /g;
     s/!+/！/g;
     s/\?+/？/g;
     s/,+/，/g;
@@ -75,8 +67,8 @@ my @tweets = sort { length($b) <=> length($a) } map {
 } grep {
     /\p{Han}{2}/ && /\p{Punct}/ && length($_) > 6
 } map {
-    split /\r?\n+/
-} uniq @new_tweets, @old_tweets;
+    split /(?:\r?\n)+/
+} (@new_tweets, @old_tweets);
 
 my $out = $app_root->catfile("corpus", "tweets.txt")->utf8;
 $out->println($_) for @tweets;
